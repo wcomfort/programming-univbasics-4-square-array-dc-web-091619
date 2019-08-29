2055f73ce829abe8f53e849c105cd4c83a56daaf
Will Comfort <comfortwb21@gmail.com>
2019-08-29 17:25:35 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   new_array = []
   counter = 0 
   while counter < array.length do 
-   new_array = array[counter] ** 2 
+   new_array.push(array[counter] ** 2)
     counter += 1
   end
   return new_array
